ef3dbf6926f66c230898654e0d6fde8733072a01
Flawid DSouza <me@flawiddsouza.com>
2022-05-14 15:17:51 +0530

    Into git

---
+++ README.md
@@ -0,0 +1,7 @@
+A more generic lightweight alternative to nodemon.
+
+#### Usage
+```bash
+npx github:flawiddsouza/watchy node app.js
+```
+where `node app.js` is the command you want to run every time a `.js` file changes in the current directory
---
+++ main.mjs
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+import { watch } from 'chokidar'
+import { spawn } from 'node:child_process'
+import kill from 'tree-kill'
+
+const args = process.argv.slice(2)
+
+if(args.length < 2) {
+    process.exit()
+}
+
+function createProcess(args) {
+    const command = args[0]
+    const commandOptions = [...args.slice(1)]
+
+    // console.log({ command, commandOptions })
+
+    const spawnedProcess = spawn(command, commandOptions)
+
+    spawnedProcess.stdout.setEncoding('utf8')
+    spawnedProcess.stdout.on('data', data => {
+        console.log(data.toString())
+    })
+
+    spawnedProcess.stderr.setEncoding('utf8')
+    spawnedProcess.stderr.on('data', data => {
+        console.error('\x1b[31m', data.toString(), '\x1b[0m')
+    })
+
+    return spawnedProcess
+}
+
+let spawnedProcess = createProcess(args)
+
+watch('*.js').on('change', async(file, stats) => {
+    kill(spawnedProcess.pid, () => {
+        spawnedProcess = null
+        console.log('Changed: ' + file)
+        console.log('Reloading...')
+        spawnedProcess = createProcess(args)
+    })
+})
